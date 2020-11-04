071bb8c75931c409f52836c32fef743a5cc5a890
Valeria Meza <valeriameza173@gmail.com>
2020-11-03 19:59:32 -0600

    added test to require title and description

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -17,6 +17,10 @@ class ProjectsController extends Controller
     public function store() 
     {
         // validate
+        request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+            ]);
 
         // persist
         Project::create(request(['title', 'description']));
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -32,4 +32,17 @@ class ProjectsTest extends TestCase
         // in this case project title
         $this->get('/projects')->assertSee($attributes['title']);
     }
+
+    /** @test */
+    public function a_project_requires_a_title()
+    {
+        $this->post('/projects', [])->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_requires_a_description()
+    {
+        $this->post('/projects', [])->assertSessionHasErrors('description');
+    }
+
 }
