4cb5ed9a4df00c9657217d5a981d202c21432f86
Valeria Meza <valeriameza173@gmail.com>
2020-11-10 15:17:54 -0600

    add class that is responsible for project activites.

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Project;
+use App\Models\Activity;
+
+
+class ProjectObserver
+{
+    /**
+     * Handle the Project "created" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        Activity::create([
+            'project_id' => $project->id,
+            'description' => 'Created'
+        ]);
+    }
+
+    /**
+     * Handle the Project "updated" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        Activity::create([
+            'project_id' => $project->id,
+            'description' => 'updated'
+        ]);
+    }
+
+    /**
+     * Handle the Project "deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "restored" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "force deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+}
