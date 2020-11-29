9c0a5fbc29485747c998b6a0cfbe345f29a30419
Valeria Meza <valeriameza173@gmail.com>
2020-11-29 15:50:44 -0600

    add when time activity stamps.

--- resources/views/projects/activity/card.blade.php
+++ resources/views/projects/activity/card.blade.php
@@ -3,6 +3,7 @@
         @foreach($project->activity as $activity)
             <li class="{{ $loop->last ? '' : 'mb-1' }}">
                 @include("projects.activity.$activity->description")
+                <span class="text-gray-200">{{ $activity->created_at->diffForHumans() }}</span>
             </li>
         @endforeach
     </ul>
